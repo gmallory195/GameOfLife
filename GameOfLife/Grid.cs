1c32b98ef30a1b717f8dceb7500baa32c1ac286b
Greg E. Mallory <gemallory@gmail.com>
2017-10-11 19:34:25 -0400

    Created grid and added test class. so far have done no testing on any of the methods to make sure anything works.

---
+++ GameOfLife/Grid.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLife
+{
+    public class Grid
+    {
+        int width;
+        int height;
+
+        bool[,] cells;
+
+        public Grid(bool[,] startingPosition)
+        {
+            this.width = startingPosition.GetLength(0);
+            this.height = startingPosition.GetLength(1);
+
+            this.cells = startingPosition;
+        }
+
+        /// <summary>
+        /// Updates the grid to the new state.
+        /// </summary>
+        public void UpdateState()
+        {
+            //
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    int neighbors = CountNeighboringTiles(i, j);
+
+                    //in the case where 2 neighbors exist, the game takes the existing state, since it never changes the state of a particular cell.
+                    bool nextState = cells[i,j];
+
+                    //if less than 2 neighbors or more than 3, the cell dies.
+                    if(neighbors < 2 || neighbors > 3)
+                    {
+                        nextState = false;
+                    }
+                    //if exactly 3 the cell will always be alive.
+                    else if(neighbors == 3)
+                    {
+                        nextState = true;
+                    }
+
+                    cells[i, j] = nextState;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Returns a number of neighbors, used to determine whether a cell is born, survives, or dies.
+        /// </summary>
+        /// <param name="xPosition">The X position of the grid that you want the game to find the number of neighbors.</param>
+        /// <param name="yPosition">The Y position of the grid that you want the game to find the number of neighbors.</param>
+        /// <returns></returns>
+        public int CountNeighboringTiles(int xPosition, int yPosition)
+        {
+            int neighbors = 0;
+            for (int i = -1; i < 1; i++)
+            {
+                int newX = xPosition + i;
+                for (int j = -1; j < 1; j++)
+                {
+                    int newY = yPosition + j;
+                    //need to make sure position in bounds
+                    if((newX < 0) || (newX > width) || (newY < 0) || (newY > height))
+                    {
+                        //one of the coordinates was out of bounds
+                        continue;
+                    }
+                    else
+                    {
+                        if (cells[newX, newY])
+                        {
+                            neighbors++;
+                        }
+                    }
+                }
+            }
+            return neighbors;
+        }
+    }
+}
---
+++ GameOfLifeTests/GridTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GameOfLife;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLife.Tests
+{
+    [TestClass()]
+    public class GridTests
+    {
+        [TestMethod()]
+        public void GridTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void UpdateStateTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CountNeighboringTilesTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
